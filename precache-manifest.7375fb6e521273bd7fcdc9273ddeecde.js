b473106c73db5a5d011a690000ccb7ed93920a67
maverick12345678 <adi.maheshwari53@gmail.com>
2020-06-19 09:33:12 +0530

    Updates

---
+++ precache-manifest.7375fb6e521273bd7fcdc9273ddeecde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4207b06b4e18cf27773a",
+    "url": "/me/static/css/main.2bea9fed.chunk.css"
+  },
+  {
+    "revision": "4207b06b4e18cf27773a",
+    "url": "/me/static/js/main.4207b06b.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "92a83dcd29dc4a346b68305be93f23e4",
+    "url": "/me/index.html"
+  }
+];
