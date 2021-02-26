4b3b59801331ee6a22f16ea82ff1920522eabf05
beardbytes <adi.maheshwari53@gmail.com>
2021-02-26 14:52:12 +0530

    Modified the language

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -21,8 +21,12 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at RedCarpet <span>March2020 - June2020</span></h2>
-                        <p>I recently worked with RedCarpet as a Full Stack Developer-Intern in the DevOps team. My major part of the work has been working with docker to containerize applications . I also worked with the Frontend Team in building new UI in React . I worked closely in firing optimised querries on the system.</p>
+                        <h2><b>RedCarpet - Full stack intern</b> <span>March2020 - June2020</span></h2>
+                        <p>I have completed working with RedCarpet as a Full Stack Intern in the DevOps team.
+                        My major part of the work has been working with docker to containerize applications and
+                        work with Airflow to schedule jobs. I also worked with the Frontend Team in building
+                        new componenets in React. I worked closely with PL/SQL in writing querries on the system.
+                        I have also a working knowledge of various version control tools like Git, BitBucket.</p>
                       </div>
                     </div>
                   </article>
@@ -32,8 +36,11 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Undergraduation at MIT WPU<span>2017-2021</span></h2>
-                        <p>I am pursuing my under-graduation studies with major in CSE(Computer Science and Engineering). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of Student Council(Student Body) in my first year of college.</p>
+                        <h2><b>MIT World Peace University</b> <span>2017-2021</span></h2>
+                        <p>I am currently in my final year and pursuing my under-graduation studies
+                        with majors in Computer Science and Engineering with <b>CGPA 9.26</b>. I have taken courses like DSA, OOPs,
+                        Computer Networks over the years and have better understanding of these subjects.
+                           I have also been part of Student Council in my first year of college.</p>
                       </div>
                     </div>
                   </article>
@@ -43,8 +50,12 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Primary and Higher Education <span>2003-2014</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 84 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p>
+                        <h2><b>Primary Education</b> <span>2003-2014</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as
+                        Physics, Chemistry & Maths with 85%. During my time at school,
+                        I have developed interest in solving complex problems of the fundamental
+                        physics which helped me to improve my understanding of any problem and
+                          also my mathematical skills to actually solve the problems.</p>
                       </div>
                     </div>
                   </article>
